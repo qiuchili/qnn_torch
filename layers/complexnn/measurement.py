Fixing the loss overflow problem

layers/complexnn/measurement.py
@@ -13,11 +13,15 @@ class ComplexMeasurement(torch.nn.Module):
             self.real_kernel = torch.nn.Parameter(torch.eye(embed_dim))
             self.imag_kernel = torch.nn.Parameter(torch.zeros(embed_dim, embed_dim))
         else:
-            self.real_kernel = torch.nn.Parameter(torch.Tensor(self.units, embed_dim))
-            self.imag_kernel = torch.nn.Parameter(torch.Tensor(self.units, embed_dim))
+            self.kernel = torch.nn.Parameter(torch.rand(self.units, embed_dim, 2))
+            normalized_kernel = F.normalize(self.kernel.view(self.units, -1), p=2, dim=1, eps=1e-10).view(self.units, embed_dim, 2)
+            self.real_kernel = normalized_kernel[:,:,0]
+            self.imag_kernel = normalized_kernel[:,:,1]
+#            self.real_kernel = torch.nn.Parameter(torch.Tensor(self.units, embed_dim))
+#            self.imag_kernel = torch.nn.Parameter(torch.Tensor(self.units, embed_dim))
 
     def forward(self, inputs, measure_operator=None):
-        batch_size = inputs[0].size(0)
+
         input_real = inputs[0]
         input_imag = inputs[1]
         
@@ -33,8 +37,10 @@ class ComplexMeasurement(torch.nn.Module):
         projector_imag = torch.matmul(imag_kernel, real_kernel.transpose(1, 2)) \
             - torch.matmul(real_kernel, imag_kernel.transpose(1, 2))
         # only real part is non-zero
-        output_real = torch.mm(input_real.view(batch_size, self.embed_dim*self.embed_dim), projector_real.view(self.units,self.embed_dim*self.embed_dim).t())\
-            - torch.mm(input_imag.view(batch_size, self.embed_dim*self.embed_dim), projector_imag.view(self.units,self.embed_dim*self.embed_dim).t())
+        # input_real.shape = [batch_size, seq_len, embed_dim, embed_dim] or [batch_size, embed_dim, embed_dim]
+        # projector_real.shape = [num_measurements, embed_dim, embed_dim]
+        output_real = torch.matmul(torch.flatten(input_real, start_dim = -2, end_dim = -1), torch.flatten(projector_real, start_dim = -2, end_dim = -1).t())\
+            - torch.matmul(torch.flatten(input_imag, start_dim = -2, end_dim = -1), torch.flatten(projector_imag, start_dim = -2, end_dim = -1).t())
     
         return output_real
     
@@ -42,10 +48,10 @@ if __name__ == '__main__':
     model = ComplexMeasurement(6, units=3)
     a = torch.randn(5,6,6)
     b = torch.randn(5,6,6)
-
+#
     y_pred = model([a,b])
     print(y_pred.shape)
-    
+#    
     
     
     

layers/complexnn/proj_measurement.py
@@ -6,7 +6,7 @@ from optimizer.pytorch_optimizer import Vanilla_Unitary
 from .measurement import ComplexMeasurement
 
 class ComplexProjMeasurement(torch.nn.Module):
-    def __init__(self, opt, embed_dim, method='ensemble'):
+    def __init__(self, opt, embed_dim, method='sample'):
         super(ComplexProjMeasurement, self).__init__()
         self.opt = opt
         self.embed_dim = embed_dim
@@ -45,7 +45,7 @@ class ComplexProjMeasurement(torch.nn.Module):
             imag_samples.append(imag_sample)
         real_samples = torch.cat(real_samples, dim=1)
         imag_samples = torch.cat(imag_samples, dim=1)
-        return [real_sample, imag_samples]
+        return [real_samples, imag_samples]
     
 if __name__ == '__main__':
     pass

models/classification/MLLM.py
@@ -42,19 +42,20 @@ class MLLM(torch.nn.Module):
         self.measurement = ComplexMeasurement(self.embedding_dim, units = 2*self.num_measurements)
         self.use_lexicon_as_measurement = opt.use_lexicon_as_measurement
         self.num_hidden_layers = opt.num_hidden_layers
-        if self.pooling_type == 'max':
-            self.dense = nn.Linear(len(self.ngram), 2)
-        elif self.pooling_type == 'average':
-            self.dense = nn.Linear(len(self.ngram), 2)
-        elif self.pooling_type == 'none':
-            self.dense = nn.Linear(len(self.ngram)*2*self.num_measurements, 2)
-        elif self.pooling_type == 'max_col':
-            self.dense = nn.Linear(2*self.num_measurements, 2)
-        elif self.pooling_type == 'average_col':
-            self.dense = nn.Linear(2*self.num_measurements, 2)
-        else:
-            print('Wrong input pooling type -- The default flatten layer is used.')
-            self.dense = nn.Linear(len(self.ngram)*2*self.num_measurements, 2)
+        self.hidden_units = 16
+#        if self.pooling_type == 'max':
+#            self.dense = nn.Linear(len(self.ngram), 2)
+#        elif self.pooling_type == 'average':
+#            self.dense = nn.Linear(len(self.ngram), 2)
+#        elif self.pooling_type == 'none':
+#            self.dense = nn.Linear(len(self.ngram)*2*self.num_measurements, 2)
+#        elif self.pooling_type == 'max_col':
+#            self.dense = nn.Linear(2*self.num_measurements, 2)
+#        elif self.pooling_type == 'average_col':
+#            self.dense = nn.Linear(2*self.num_measurements, 2)
+#        else:
+#            print('Wrong input pooling type -- The default flatten layer is used.')
+#            self.dense = nn.Linear(len(self.ngram)*2*self.num_measurements, 2)
         
     def forward(self, input_seq):
         """
@@ -75,8 +76,12 @@ class MLLM(torch.nn.Module):
                 n_gram_weight = n_gram(weights)
                 n_gram_weight = self.activation(n_gram_weight)
                 [sentence_embedding_real, sentence_embedding_imag] = self.mixture([real_n_gram_embed, imag_n_gram_embed, n_gram_weight])
-                [seq_embedding_real, seq_embedding_imag] = self.proj_measurement[i]([sentence_embedding_real, sentence_embedding_imag])
-            [sentence_embedding_real, sentence_embedding_imag] = self.final_mixture([seq_embedding_real, seq_embedding_imag])
+                [seq_embedding_real, seq_embedding_imag] = self.proj_measurements[i]([sentence_embedding_real, sentence_embedding_imag])
+            
+            real_n_gram_embed = n_gram(seq_embedding_real)
+            imag_n_gram_embed = n_gram(seq_embedding_imag)
+            [sentence_embedding_real, sentence_embedding_imag] = self.mixture([real_n_gram_embed, imag_n_gram_embed, n_gram_weight])
+#            [sentence_embedding_real, sentence_embedding_imag] = self.final_mixture([seq_embedding_real, seq_embedding_imag])
             mea_operator = None
             if self.use_lexicon_as_measurement:
                 amplitude_measure_operator, phase_measure_operator = self.complex_embed.sample(self.num_measurements)
@@ -103,6 +108,8 @@ class MLLM(torch.nn.Module):
             print('Wrong input pooling type -- The default flatten layer is used.')
             probs = torch.flatten(probs, start_dim=1, end_dim=2)
         
-        output = self.dense(probs)
+        probs = torch.flatten(probs, start_dim = -2, end_dim = -1)
+        probs = nn.Linear(probs.shape[-1],self.hidden_units)(probs)
+        output = nn.Linear(self.hidden_units, 2)(probs)
         
         return output

run.py
@@ -10,13 +10,6 @@ import numpy as np
 import preprocess.embedding
 import torch
 import torch.nn as nn
-from loss import *
-import pandas as pd
-
-from loss.pairwise_loss import *
-from loss.triplet_loss import *
-import loss.pairwise_loss
-import loss.triplet_loss
 import models
 
 def run(params):
@@ -45,7 +38,7 @@ def run(params):
             targets = sample_batched['y'].long().to(params.device)
             outputs = model(inputs)
             loss = criterion(outputs, torch.max(targets, 1)[1])
-            loss.backward()
+            loss.backward(retain_graph=True)
             optimizer.step()
 
             n_correct = (torch.argmax(outputs, -1) == torch.argmax(targets, -1)).sum().item()