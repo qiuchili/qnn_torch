Resolving dtype inconsistency error and fixing the bug on wrong dimension input for Softmax()

layers/complexnn/ngram.py
@@ -14,11 +14,11 @@ class NGram(torch.nn.Module):
     e.g. input_shape = (None,10,3) gram_n = 5, axis = 1 ==> output_shape = (None,10,5,3)
     
     '''
-    def __init__(self, opt, gram_n=3, dim=1):
+    def __init__(self, gram_n=3, dim=1, device = torch.device('cpu')):
         super(NGram, self).__init__()
-        self.opt = opt
         self.gram_n = gram_n
         self.dim = dim
+        self.device = device
         
     def forward(self, inputs):
         
@@ -29,8 +29,8 @@ class NGram(torch.nn.Module):
         total_padded_len = self.gram_n - 1
         right_padded_len = left_padded_len = int(total_padded_len/2)
         
-        left_padded_zeros = torch.zeros(batch_size, left_padded_len, embed_dim, dtype=torch.float).to(self.opt.device)
-        right_padded_zeros = torch.zeros(batch_size, left_padded_len, embed_dim, dtype=torch.float).to(self.opt.device)
+        left_padded_zeros = torch.zeros(batch_size, left_padded_len, embed_dim).to(self.device)
+        right_padded_zeros = torch.zeros(batch_size, right_padded_len, embed_dim).to(self.device)
         
         inputs = torch.cat([left_padded_zeros, inputs, right_padded_zeros], dim=self.dim)
 
@@ -40,7 +40,7 @@ class NGram(torch.nn.Module):
         for i in range(out_n):
             slice_begin_index = i
             slice_end_index = i + self.gram_n
-            slice_index = torch.tensor(np.arange(slice_begin_index, slice_end_index), dtype=torch.long).to(self.opt.device)
+            slice_index = torch.tensor(np.arange(slice_begin_index, slice_end_index), dtype=torch.long).to(self.device)
             l = torch.index_select(inputs, self.dim, index=slice_index)
             list_of_ngrams.append(torch.unsqueeze(l, dim=self.dim+1))
                 

layers/complexnn/proj_measurement.py
@@ -6,12 +6,12 @@ from optimizer.pytorch_optimizer import Vanilla_Unitary
 from .measurement import ComplexMeasurement
 
 class ComplexProjMeasurement(torch.nn.Module):
-    def __init__(self, opt, embed_dim, method='sample'):
+    def __init__(self, opt, embed_dim, method='sample', device = torch.device('cpu')):
         super(ComplexProjMeasurement, self).__init__()
         self.opt = opt
         self.embed_dim = embed_dim
         self.method = method
-        self.measurement = ComplexMeasurement(embed_dim, units=embed_dim, ortho_init=True)
+        self.measurement = ComplexMeasurement(embed_dim, units=embed_dim, ortho_init=True, device = device)
 
     def forward(self, inputs):
 
@@ -36,8 +36,8 @@ class ComplexProjMeasurement(torch.nn.Module):
             output = self.measurement([chunks_real[i], chunks_imag[i]])
             if self.method == 'sample':
                 sampled_indice = output.multinomial(1).squeeze(1)
-                real_sample = torch.index_select(self.measurement.real_kernel, 0, sampled_indice).unsqueeze(1)
-                imag_sample = torch.index_select(self.measurement.imag_kernel, 0, sampled_indice).unsqueeze(1)
+                real_sample = torch.index_select(self.measurement.kernel[:,:,0], 0, sampled_indice).unsqueeze(1)
+                imag_sample = torch.index_select(self.measurement.kernel[:,:,1], 0, sampled_indice).unsqueeze(1)
             elif self.method == 'ensemble':
                 real_sample = torch.mm(output, self.measurement.real_kernel).unsqueeze(1)
                 imag_sample = torch.mm(output, self.measurement.imag_kernel).unsqueeze(1)

optimizer/pytorch_optimizer.py
@@ -28,11 +28,11 @@ class Vanilla_Unitary(Optimizer):
         and learning rate respectively.
     """
 
-    def __init__(self, params, lr=required):
+    def __init__(self, params, lr=required, device = torch.device('cpu')):
         if lr is not required and lr < 0.0:
             raise ValueError("Invalid learning rate: {}".format(lr))
 
-
+        self.device = device
         defaults = dict(lr=lr)
         super(Vanilla_Unitary, self).__init__(params, defaults)
 
@@ -66,8 +66,8 @@ class Vanilla_Unitary(Optimizer):
                     continue
                 
                 d_p = p.grad.data #G
-                G = d_p[:,:,0].numpy()+1j* d_p[:,:,1].numpy()               
-                W = p.data[:,:,0].numpy()+1j* p.data[:,:,1].numpy()   
+                G = d_p[:,:,0].cpu().numpy()+1j* d_p[:,:,1].cpu().numpy()               
+                W = p.data[:,:,0].cpu().numpy()+1j* p.data[:,:,1].cpu().numpy()   
                 
                 #A = G^H W - W^H G
                 A_skew = np.matmul(np.matrix.getH(G),W) - np.matmul(np.matrix.getH(W),G)
@@ -78,11 +78,11 @@ class Vanilla_Unitary(Optimizer):
                 cayley_numer = identity - (lr/2)* A_skew
                 W_new = np.matmul(np.matmul(cayley_denom,cayley_numer),W)
                 
-                p_new_real = torch.tensor(W_new.real)
-                p_new_imag = torch.tensor(W_new.imag)
+                p_new_real = torch.tensor(W_new.real, dtype = torch.float).to(self.device)
+                p_new_imag = torch.tensor(W_new.imag, dtype = torch.float).to(self.device)
                 p_new = torch.cat((p_new_real.unsqueeze(2),p_new_imag.unsqueeze(2)),2)
 
-                p.data = p_new.float()
+                p.data = p_new
 
 
         return loss