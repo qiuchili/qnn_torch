923e14bc9adef2083eefcedcc6eba14b82cbc2c1
WhereIsMyHead <gene_zhangchen@163.com>
2019-01-17 20:06:32 +0800

    Update run_temp.py

--- run_temp.py
+++ run_temp.py
@@ -10,7 +10,6 @@ import preprocess.embedding
 import torch
 import torch.nn as nn
 import models
-from tqdm import tqdm,trange
 
 def run(params):
     model = models.setup(params)
@@ -18,64 +17,51 @@ def run(params):
     criterion = nn.CrossEntropyLoss()    
     
     optimizer = torch.optim.RMSprop(model.parameters(), lr=params.lr)
-    
-    t = trange(params.sample_num['train'])
-    
+
     max_test_acc = 0.
     for i in range(params.epochs):
         print('epoch: ', i)
         train_accs = []
-        losses = []
-        
-        
+        train_losses = []
         for _i, sample_batched in enumerate(params.reader.get_train(iterable = True)):
-            
             model.train()
             optimizer.zero_grad()
             inputs = sample_batched['X'].to(params.device)
             targets = sample_batched['y'].to(params.device)
             if params.strategy == 'multi-task':
                 senti_outputs, senti_targets, outputs = model(inputs)
-                loss = criterion(outputs, torch.max(targets, 1)[1]) + params.gamma*criterion(senti_outputs, senti_targets)
+                loss = criterion(outputs, targets.argmax(1)) + params.gamma*criterion(senti_outputs, senti_targets)
             else:
                 outputs = model(inputs)
-                loss = criterion(outputs, torch.max(targets, 1)[1])
+                loss = criterion(outputs, targets.argmax(1))
             loss.backward()
             optimizer.step()
-            n_correct = (torch.argmax(outputs, -1) == torch.argmax(targets, -1)).sum().item()
+            n_correct = (outputs.argmax(1) == targets.argmax(1)).sum().item()
             n_total = len(outputs)
             train_acc = n_correct / n_total
-            t.update(params.batch_size)
-            t.set_postfix(loss=loss.item(), train_acc=train_acc)
             train_accs.append(train_acc)
-            losses.append(loss.item())
-        
-        t.clear()
-        
-        avg_train_acc = np.mean(train_accs)
-        avg_loss = np.mean(losses)
-        print('average train_acc: {}, average train_loss: {}'.format(avg_train_acc, avg_loss))
-        
-        test_losses = []
-        n_correct = 0
-        n_total = 0
-        for _i, sample_batched in enumerate(params.reader.get_test(iterable = True)):
-            test_inputs = sample_batched['X'].to(params.device)
-            test_targets = sample_batched['y'].to(params.device)
-            with torch.no_grad():
-                if params.strategy == 'multi-task':
-                    _, _, test_outputs = model(test_inputs.long())
-                else:
-                    test_outputs = model(test_inputs.long())
-                n_correct += (torch.argmax(test_outputs, -1) == torch.argmax(test_targets, -1)).sum().item()
-                n_total += len(test_outputs)
-                loss = criterion(test_outputs, torch.max(test_targets, 1)[1])
-            test_losses.append(loss.item())
-        test_acc = n_correct / n_total
-        if test_acc > max_test_acc:
-            max_test_acc = test_acc
-        avg_test_loss = np.mean(test_losses)
-        print('test_acc: {}, test_loss: {}\n\n\n'.format(test_acc, avg_test_loss.item()))
+            train_losses.append(loss.item())
+
+            if _i % 50 == 0:
+                model.eval()
+                avg_train_acc = np.mean(train_accs)
+                avg_train_loss = np.mean(train_losses)
+                t_n_correct = 0
+                t_n_total = 0
+                for t_sample_batched in params.reader.get_test(iterable = True):
+                    t_inputs = t_sample_batched['X'].to(params.device)
+                    t_targets = t_sample_batched['y'].to(params.device)
+                    with torch.no_grad():
+                        if params.strategy == 'multi-task':
+                            _, _, t_outputs = model(t_inputs)
+                        else:
+                            t_outputs = model(t_inputs)
+                        t_n_correct += (t_outputs.argmax(1) == t_targets.argmax(1)).sum().item()
+                        t_n_total += len(t_outputs)
+                test_acc = n_correct / n_total
+                if test_acc > max_test_acc:
+                    max_test_acc = test_acc
+                print('average train_acc: {}, average train_loss: {}, test_acc: {}'.format(avg_train_acc, avg_train_loss, test_acc))
     
     print('max test_acc: {}'.format(max_test_acc))
 
