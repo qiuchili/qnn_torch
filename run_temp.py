8d06021496c621bce75baf7d8ba9ac00d42f1206
WhereIsMyHead <gene_zhangchen@163.com>
2019-01-17 13:15:25 +0800

    Fix test acc.

--- run_temp.py
+++ run_temp.py
@@ -21,6 +21,7 @@ def run(params):
     
     t = trange(params.sample_num['train'])
     
+    max_test_acc = 0.
     for i in range(params.epochs):
         print('epoch: ', i)
         train_accs = []
@@ -34,11 +35,11 @@ def run(params):
             inputs = sample_batched['X'].to(params.device)
             targets = sample_batched['y'].to(params.device)
             if params.strategy == 'multi-task':
-                outputs = model(inputs)
-                loss = criterion(outputs, torch.max(targets, 1)[1])
-            else:
                 senti_outputs, senti_targets, outputs = model(inputs)
                 loss = criterion(outputs, torch.max(targets, 1)[1]) + 0.2*criterion(senti_outputs, senti_targets)
+            else:
+                outputs = model(inputs)
+                loss = criterion(outputs, torch.max(targets, 1)[1])
             loss.backward()
             optimizer.step()
             n_correct = (torch.argmax(outputs, -1) == torch.argmax(targets, -1)).sum().item()
@@ -55,22 +56,25 @@ def run(params):
         avg_loss = np.mean(losses)
         print('average train_acc: {}, average train_loss: {}'.format(avg_train_acc, avg_loss))
         
-        test_accs = []
         test_losses = []
+        n_correct = 0
+        n_total = 0
         for _i, sample_batched in enumerate(params.reader.get_test(iterable = True)):
             test_inputs = sample_batched['X'].to(params.device)
             test_targets = sample_batched['y'].to(params.device)
             with torch.no_grad():
                 test_outputs = model(test_inputs.long())
-                n_correct = (torch.argmax(test_outputs, -1) == torch.argmax(test_targets, -1)).sum().item()
-                n_total = len(test_outputs)
-                test_acc = n_correct / n_total
+                n_correct += (torch.argmax(test_outputs, -1) == torch.argmax(test_targets, -1)).sum().item()
+                n_total += len(test_outputs)
                 loss = criterion(test_outputs, torch.max(test_targets, 1)[1])
-            test_accs.append(test_acc)
             test_losses.append(loss.item())
-        avg_test_acc = np.mean(test_accs)
+        test_acc = n_correct / n_total
+        if test_acc > max_test_acc:
+            max_test_acc = test_acc
         avg_test_loss = np.mean(test_losses)
-        print('test_acc: {}, test_loss: {}\n\n\n'.format(avg_test_acc, avg_test_loss.item()))
+        print('test_acc: {}, test_loss: {}\n\n\n'.format(test_acc, avg_test_loss.item()))
+    
+    print('max test_acc: {}'.format(max_test_acc))
 
 
 
